b06ebe1a63f0cbe70ea9a1af40b1aa7d1ffc4350
Alex289 <mccr4fthd@gmail.com>
2021-09-22 12:55:24 +0200

    Fix age replacement

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import profilePic from '../public/static/images/konietzko_alexander.jpg';
 
 export default function Index() {
   const { t } = useTranslation();
+  const age = new Date().getFullYear() - 2002;
   return (
     <Layout>
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
@@ -35,9 +36,7 @@ export default function Index() {
           {t('about')}
         </h3>
         <h2 className="text-gray-600 dark:text-gray-400 mb-16">
-          <p className="mb-6">
-            {t('about-1').replace('$AGE', new Date().getFullYear() - 2002)}
-          </p>
+          <p className="mb-6">{t('about-1').replace('$AGE', age.toString())}</p>
           <p className="mb-6">{t('about-2')}</p>
           <p>{t('about-3')}</p>
         </h2>
