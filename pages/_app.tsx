e757ca79fc1ba89e8a49ff2ded178a34f445059b
Alex289 <mccr4fthd@gmail.com>
2021-09-24 11:35:42 +0200

    Add nprogress loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from 'next-themes';
+import Router from 'next/router';
 
-import '../styles/global.css';
+import { ThemeProvider } from 'next-themes';
+import NProgress from 'nprogress';
+
+import '@/styles/global.css';
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => {
+  NProgress.start();
+});
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
