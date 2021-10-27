ddca03e275015fcb4c04518ffe655f97aa5f642b
Alex289 <mccr4fthd@gmail.com>
2021-10-27 13:53:10 +0200

    Try adding basic auth to dashboard

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,6 +1,29 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export function middleware() {
+export function middleware(req: NextRequest) {
+  if (req.page.name === '/dashboard') {
+    const basicAuth = req.headers.get('authorization');
+
+    if (basicAuth) {
+      const auth = basicAuth.split(' ')[1];
+      const [user, pwd] = Buffer.from(auth, 'base64').toString().split(':');
+
+      if (
+        user === process.env.AUTH_ADMIN_USERNAME &&
+        pwd === process.env.AUTH_ADMIN_PASSWORD
+      ) {
+        return NextResponse.next();
+      }
+    }
+
+    return new Response('Auth required', {
+      status: 401,
+      headers: {
+        'WWW-Authenticate': 'Basic realm="Secure Area"',
+      },
+    });
+  }
+
   const ContentSecurityPolicy = `
     default-src 'self';
     script-src 'self' 'unsafe-eval' 'unsafe-inline';
