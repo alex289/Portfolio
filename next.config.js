39290242e41f7e14fc3dba2d7664a3e8ae4ef631
Alex289 <mccr4fthd@gmail.com>
2022-02-15 23:11:40 +0100

    Update deps

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ module.exports = {
   webpack: (config, { dev, isServer }) => {
     if (!dev && !isServer) {
       Object.assign(config.resolve.alias, {
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
         react: 'preact/compat',
         'react-dom/test-utils': 'preact/test-utils',
         'react-dom': 'preact/compat',
