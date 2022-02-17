65032a61a8fe6fa4ec4520a87eba91e1d1e0ff05
Alex289 <mccr4fthd@gmail.com>
2022-02-17 20:26:51 +0100

    Update NextJs ✨

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
 export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
   theme: {
     colorScheme: 'auto',
     brandColor: '',
