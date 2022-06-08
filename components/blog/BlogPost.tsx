1b206897ab134f66c4c6f7c2a741a4f165c5d6a0
Alex289 <mccr4fthd@gmail.com>
2022-06-08 21:02:19 +0200

    feat: :sparkles: Add date to blog showcase

--- components/blog/BlogPost.tsx
+++ components/blog/BlogPost.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import useSWR from 'swr';
+import { parseISO, format } from 'date-fns';
 
 import fetcher from '@/lib/fetcher';
 
@@ -10,7 +11,8 @@ export default function BlogPost({
   title,
   summary,
   slug,
-}: Pick<Blog, 'title' | 'summary' | 'slug'>) {
+  publishedAt,
+}: Pick<Blog, 'title' | 'summary' | 'slug' | 'publishedAt'>) {
   const { data } = useSWR<Views>(`/api/views/${slug}`, fetcher);
   const views = data?.total;
 
@@ -22,8 +24,9 @@ export default function BlogPost({
             <h3 className="w-full mb-2 text-lg font-medium text-gray-900 md:text-xl dark:text-gray-100">
               {title}
             </h3>
-            <p className="w-32 mb-4 text-left text-gray-900 dark:text-[#c2c2c2] md:text-right md:mb-0">
-              {`${views ? new Number(views).toLocaleString() : '–––'} views`}
+            <p className="w-full mb-4 text-left text-gray-900 dark:text-[#c2c2c2] md:text-right md:mb-0">
+              {`${views ? new Number(views).toLocaleString() : '–––'} views`} |{' '}
+              {format(parseISO(publishedAt), 'MMMM dd, yyyy')}
             </p>
           </div>
           <p className="text-gray-600 dark:text-[#c2c2c2]">{summary}</p>
