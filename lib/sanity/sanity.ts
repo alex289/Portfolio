60175df22ac0f3426f33d6bd9706d58ccb70ff30
Alex289 <mccr4fthd@gmail.com>
2022-06-23 12:32:33 +0200

    Remove redundant sanity stuff

--- lib/sanity/sanity.ts
+++
@@ -1,9 +0,0 @@
-import createImageUrlBuilder from '@sanity/image-url';
-import { SanityImageSource } from '@sanity/image-url/lib/types/types';
-
-import { sanityConfig } from './sanity-config';
-
-export const imageBuilder = createImageUrlBuilder(sanityConfig);
-
-export const urlForImage = (source: SanityImageSource) =>
-  imageBuilder.image(source).auto('format').fit('max');
