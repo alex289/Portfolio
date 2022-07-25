15d8cb56e3ff5d1d81a0b257cc1f56835eee00e3
Alex289 <mccr4fthd@gmail.com>
2022-07-25 19:47:03 +0200

    Does this work

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { isValidRequest } from '@sanity/webhook';
+import { isValidSignature, SIGNATURE_HEADER_NAME } from '@sanity/webhook';
 import { sanityClient } from '@/lib/sanity/sanity-server';
 import { postUpdatedQuery } from '@/lib/sanity/queries';
 
@@ -17,12 +17,19 @@ export default async function handler(
       .json({ message: 'Missing sanity studio revalidate secret' });
   }
 
-  req.body = req.body.toString();
-  if (!isValidRequest(req, process.env.SANITY_STUDIO_REVALIDATE_SECRET)) {
+  const signature = req.headers[SIGNATURE_HEADER_NAME] as string;
+  const body = await readBody(req); // Read the body into a string
+  if (
+    !isValidSignature(
+      body,
+      signature,
+      process.env.SANITY_STUDIO_REVALIDATE_SECRET
+    )
+  ) {
     return res.status(401).json({ message: 'Invalid request' });
   }
 
-  const { _id: id } = req.body;
+  const { _id: id } = JSON.parse(body);
   if (typeof id !== 'string' || !id) {
     return res.status(400).json({ message: 'Invalid _id' });
   }
@@ -41,4 +48,20 @@ export default async function handler(
       return res.status(500).json({ message: 'Unknown error' });
     }
   }
+}
+
+// Next.js will by default parse the body, which can lead to invalid signatures
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+async function readBody(readable: any) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === 'string' ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks).toString('utf8');
 }
