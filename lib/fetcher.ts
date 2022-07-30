ac68c50af0420eee24d9397b56512843beb95514
Alex289 <mccr4fthd@gmail.com>
2022-07-30 17:59:15 +0200

    refactor: :art: Remove axios

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,5 +1,8 @@
-import axios from 'axios';
-
-const fetcher = (url: string) => axios.get(url).then((res) => res.data);
-
-export default fetcher;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async function fetcher<JSON = any>(
+  input: RequestInfo,
+  init?: RequestInit
+): Promise<JSON> {
+  const res = await fetch(input, init);
+  return res.json();
+}
