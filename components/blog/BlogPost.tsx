1c740a04e214dfc91891efbff2dd319d4ae4be9e
Alex289 <mccr4fthd@gmail.com>
2022-08-02 08:26:27 +0200

    feat: :sparkles: Filter posts on tag click

--- components/blog/BlogPost.tsx
+++ components/blog/BlogPost.tsx
@@ -19,33 +19,35 @@ export default function BlogPost({ title, excerpt, slug, date, tags }: Props) {
   const views = data?.total;
 
   return (
-    <Link href={`/blog/${slug}`}>
-      <a className="w-full">
-        <div className="mb-8 w-full">
-          <div className="flex flex-col justify-between md:flex-row">
-            <h3 className="mb-2 w-full text-lg font-medium text-gray-900 dark:text-gray-100 md:text-xl">
+    <div className="w-full">
+      <div className="mb-8 w-full">
+        <div className="flex flex-col justify-between md:flex-row">
+          <Link href={`/blog/${slug}`}>
+            <a className="mb-2 w-full text-lg font-medium text-gray-900 dark:text-gray-100 md:text-xl">
               {title}
-            </h3>
-            <p className="mb-4 min-w-fit max-w-full text-left text-gray-900 dark:text-[#c2c2c2] md:mb-0 md:text-right">
-              {`${views ? new Number(views).toLocaleString() : '–––'} views`} |{' '}
-              {format(parseISO(date), 'MMMM dd, yyyy')}
-            </p>
-          </div>
-          <p className="text-gray-600 dark:text-[#c2c2c2]">{excerpt}</p>
-          <div className="mt-1 flex">
-            {tags &&
-              tags.map((tag, key) => {
-                return (
-                  <div
-                    key={key}
-                    className="mx-2 text-purple-700 dark:text-purple-500">
-                    {tag}
-                  </div>
-                );
-              })}
-          </div>
+            </a>
+          </Link>
+          <p className="mb-4 min-w-fit max-w-full text-left text-gray-900 dark:text-[#c2c2c2] md:mb-0 md:text-right">
+            {`${views ? new Number(views).toLocaleString() : '–––'} views`} |{' '}
+            {format(parseISO(date), 'MMMM dd, yyyy')}
+          </p>
         </div>
-      </a>
-    </Link>
+        <p className="text-gray-600 dark:text-[#c2c2c2]">{excerpt}</p>
+        <div className="mt-1 flex">
+          {tags &&
+            tags.map((tag, key) => {
+              return (
+                <div
+                  key={key}
+                  className="mx-2 text-purple-700 dark:text-purple-500">
+                  <Link href={`/blog?search=${tag}&filter=tag`} replace shallow>
+                    {tag}
+                  </Link>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </div>
   );
 }
