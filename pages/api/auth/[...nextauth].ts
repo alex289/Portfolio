17cb6fd68a0b05b6d898559ece00befbf33111f0
Alex289 <mccr4fthd@gmail.com>
2022-08-07 20:09:27 +0200

    refactor: :bug: Add correct refresh token by google

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -17,13 +17,7 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      authorization: {
-        params: {
-          prompt: 'consent',
-          access_type: 'offline',
-          response_type: 'code',
-        },
-      },
+      accessTokenUrl: 'https://oauth2.googleapis.com/token',
     }),
   ],
   callbacks: {
