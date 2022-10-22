856a3866c6b35df0fe7f6288395dd13d4baf6bd4
alex289 <mccr4fthd@gmail.com>
2022-10-22 17:30:22 +0200

    feat: Update eslint config and fix styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -73,7 +73,7 @@ const Index: NextPage<Props> = ({ fallbackData }) => {
           </div>
           <Link
             href="/blog"
-            className="mt-8 mb-16 flex h-6 cursor-pointer rounded-lg leading-7 text-gray-600 transition-all hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-200">
+            className="mt-8 mb-16 flex h-6 cursor-pointer items-center rounded-lg leading-7 text-gray-600 transition-all hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-200">
             {t('index-page.posts.read-all')}
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -98,23 +98,23 @@ const Index: NextPage<Props> = ({ fallbackData }) => {
           <h2 className="text-gray-600 dark:text-gray-200">
             <Project fallbackData={fallbackData} />
           </h2>
-          <Link href="/projects">
-            <div className="mt-4 mb-16 flex h-6 cursor-pointer rounded-lg leading-7 text-gray-600 transition-all hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-200">
-              {t('projects.see-more')}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="ml-1 h-6 w-6">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-                />
-              </svg>
-            </div>
+          <Link
+            href="/projects"
+            className="mt-4 mb-16 flex h-6 cursor-pointer items-center rounded-lg leading-7 text-gray-600 transition-all hover:text-gray-800 dark:text-gray-300 dark:hover:text-gray-200">
+            {t('projects.see-more')}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="ml-1 h-6 w-6">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+              />
+            </svg>
           </Link>
         </div>
       </Suspense>
