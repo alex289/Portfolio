f25185bd3a0584618ed2bc44f24926b6495ffa50
alex289 <mccr4fthd@gmail.com>
2025-08-03 22:56:12 +0200

    fix: Try node postgres

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,8 +1,11 @@
 import 'server-only';
 
 import { sql } from '@vercel/postgres';
-import { drizzle } from 'drizzle-orm/vercel-postgres';
+import { drizzle as pgDrizzle } from 'drizzle-orm/node-postgres';
+import { drizzle as vercelDrizzle } from 'drizzle-orm/vercel-postgres';
 
 import * as schema from './schema';
 
-export const db = drizzle(sql, { schema });
+export const db = process.env.LOCAL_DB
+  ? pgDrizzle(sql, { schema })
+  : vercelDrizzle(sql, { schema });
