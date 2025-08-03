66a426c7a6e1a3e38dd948c8f090f3eb654d8bd1
alex289 <mccr4fthd@gmail.com>
2025-08-03 23:01:16 +0200

    fix: Fix node postgres setup

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -4,8 +4,9 @@ import { sql } from '@vercel/postgres';
 import { drizzle as pgDrizzle } from 'drizzle-orm/node-postgres';
 import { drizzle as vercelDrizzle } from 'drizzle-orm/vercel-postgres';
 
+import env from '@/env.mjs';
 import * as schema from './schema';
 
 export const db = process.env.LOCAL_DB
-  ? pgDrizzle(sql, { schema })
+  ? pgDrizzle(env.POSTGRES_URL, { schema })
   : vercelDrizzle(sql, { schema });
