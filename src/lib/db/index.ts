ff074ffd334ae2a58d6e7ede7ade551eb03c0742
alex289 <mccr4fthd@gmail.com>
2026-03-18 23:40:13 +0100

    feat: Replace vercel postgres with neon serverless

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,8 +1,9 @@
 import 'server-only';
 
-import { sql } from '@vercel/postgres';
-import { drizzle } from 'drizzle-orm/vercel-postgres';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
 
 import * as schema from './schema';
 
+const sql = neon(process.env.POSTGRES_URL!);
 export const db = drizzle(sql, { schema });
